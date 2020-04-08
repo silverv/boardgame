8fc272b157daaf177c751683f81df68b5994b944
Silver <silver.valdvee@gmail.com>
2020-04-08 15:28:45 +0300

    Fix plant select query

--- serversrc/plants.php
+++ serversrc/plants.php
@@ -2,7 +2,7 @@
 <?php
 $dbconn = pg_connect("host=13.53.41.225 dbname=silverphp user=silverphp password=5KXhj!zOw6sZ70IwTe@5")
     or die('Could not connect: ' . pg_last_error());
-$query = 'SELECT * FROM public.planet';
+$query = 'SELECT * FROM public.plant';
 
 ?>
 
