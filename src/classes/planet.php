a4ea64174eb4e9734252e5ed7e9f0693d5bed05b
Silver <silver.valdvee@gmail.com>
2020-04-09 16:02:45 +0300

    Add styles

--- src/classes/planet.php
+++ src/classes/planet.php
@@ -1,16 +1,13 @@
 <?php
     class Planet {
-        private $name;
-        private $size;
-        private $hasVolcanicActivity;
-        private $type;
-        private $climate;
-        private $hasWater;
-        private $plants;
-
-        /**
-         * Planet constructor.
-         */
+        public $name;
+        public $size;
+        public $hasVolcanicActivity;
+        public $type;
+        public $climate;
+        public $hasWater;
+        public $plants;
+    
         public function __construct() {
             $this->name = '';
             $this->size = 1;
@@ -20,125 +17,8 @@
             $this->hasWater = false;
             $this->plants = [];
         }
-
-        /**
-         * @return string
-         */
-        public function getName()
-        {
-            return $this->name;
-        }
-
-        /**
-         * @param string $name
-         */
-        public function setName($name)
-        {
-            $this->name = $name;
-        }
-
-        /**
-         * @return float
-         */
-        public function getSize()
-        {
-            return $this->size;
-        }
-
-        /**
-         * @param float $size
-         */
-        public function setSize($size)
-        {
-            $this->size = $size;
-        }
-
-        /**
-         * @return boolean
-         */
-        public function getHasVolcanicActivity()
-        {
-            return $this->hasVolcanicActivity;
-        }
-
-        /**
-         * @param boolean $hasVolcanicActivity
-         */
-        public function setHasVolcanicActivity($hasVolcanicActivity)
-        {
-            $this->hasVolcanicActivity = $hasVolcanicActivity;
-        }
-
-        /**
-         * @return string
-         */
-        public function getType()
-        {
-            return $this->type;
-        }
-
-        /**
-         * @param string $type
-         */
-        public function setType($type)
-        {
-            $this->type = $type;
-        }
-
-        /**
-         * @return string
-         */
-        public function getClimate()
-        {
-            return $this->climate;
-        }
-
-        /**
-         * @param string $climate
-         */
-        public function setClimate($climate)
-        {
-            $this->climate = $climate;
-        }
-
-        /**
-         * @return boolean
-         */
-        public function getHasWater()
-        {
-            return $this->hasWater;
-        }
-
-        /**
-         * @param boolean $hasWater
-         */
-        public function setHasWater($hasWater)
-        {
-            $this->hasWater = $hasWater;
-        }
-
-
-        /**
-         * @param Plant $plant
-         */
         public function addPlant($plant){
             array_push($this->plants, $plant);
-        }
-
-        /**
-         * @return array
-         */
-        public function getPlants()
-        {
-            return $this->plants;
-        }
-
-        /**
-         * @param array $plants
-         */
-        public function setPlants($plants)
-        {
-            $this->plants = $plants;
         }
     }
 ?>
--- src/classes/plant.php
+++ src/classes/plant.php
@@ -1,70 +1,12 @@
 <?php
     class Plant {
-        private $name;
-        private $latinName;
-        private $isPlanted;
-        private $seeds;
-        private $description;
-        private $imageURL;
+        public $name;
+        public $latinName;
+        public $isPlanted;
+        public $seeds;
+        public $description;
+        public $imageURL;
 
-        /**
-         * @return string
-         */
-        public function getDescription()
-        {
-            return $this->description;
-        }
-
-        /**
-         * @param string $description
-         */
-        public function setDescription($description)
-        {
-            $this->description = $description;
-        }
-
-        /**
-         * @return string
-         */
-        public function getImageURL()
-        {
-            return $this->imageURL;
-        }
-
-        /**
-         * @param string $imageURL
-         */
-        public function setImageURL($imageURL)
-        {
-            $this->imageURL = $imageURL;
-        }
-
-        /**
-         * @return int
-         */
-        public function getSeeds()
-        {
-            return $this->seeds;
-        }
-
-        /**
-         * @return boolean
-         */
-        public function getIsPlanted()
-        {
-            return $this->isPlanted;
-        }
-
-        /**
-         * @param boolean $isPlanted
-         */
-        public function setIsPlanted($isPlanted)
-        {
-            $this->isPlanted = $isPlanted;
-        }
-        /**
-         * Plant constructor.
-         */
         public function __construct($name, $latinName, $isPlanted, $seeds)
         {
             $this->name = $name;
@@ -72,46 +14,7 @@
             $this->isPlanted = $isPlanted;
             $this->seeds = $seeds;
             $this->description='Lorem ipsum dolor sim amet';
-            $this->imageURL='../img/brown_mud_dry_1k_jpg/brown_mud_dry_nor_1k.jpg';
-        }
-        /**
-         * @return string
-         */
-        public function getName()
-        {
-            return $this->name;
-        }
-
-        /**
-         * @param string $name
-         */
-        public function setName($name)
-        {
-            $this->name = $name;
-        }
-
-        /**
-         * @return string
-         */
-        public function getLatinName()
-        {
-            return $this->latinName;
-        }
-
-        /**
-         * @param string $latinName
-         */
-        public function setLatinName($latinName)
-        {
-            $this->latinName = $latinName;
-        }
-
-        /**
-         * @param int $seeds
-         */
-        public function setSeeds($seeds)
-        {
-            $this->seeds = $seeds;
+            $this->imageURL='img/nature-sky-green-cactus-110437.jpg';
         }
     }
 ?>
