6527af9d6bd3f9a01a0706f988f63fbcc31c8410
Roman Semenovych <romannsem@users.noreply.github.com>
2020-05-02 15:32:11 +0300

    add routs to the web

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', 'InfoController@index')->name('index');
+Route::get('/about', 'InfoController@about')->name('about');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/game', 'GameController@index')->name('game')->middleware('auth');
+Route::get('/game/{user}', 'GameController@get_game')->name('api.get_game')->middleware('auth');
+Route::put('/move/{user}', 'GameController@move')->name('api.move')->middleware('auth');
+
+Route::put('/user/update/{user}', 'UserController@update')->name('user.update')->middleware('auth');
